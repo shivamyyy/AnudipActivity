32590ad4bffddd515552b0ea40b6e5326ab1ac8a
shivamyyy <153904127+shivamyyy@users.noreply.github.com>
2025-01-14 16:15:37 +0530

    Activity3.java

--- Activity3.java
+++ Activity3.java
@@ -1,50 +1,59 @@
-Not Solved
 package lab;
 
 class Animal {
-	private String name;
+    private String name;
 
-	public Animal(String name) {
-		this.name = name;
-	}
+    public Animal(String name) {
+        this.name = name;
+    }
 
-	public void sound() {
-		System.out.println("Animal makes a sound");
-	}
+    public void sound() {
+        System.out.println("Animal makes a sound");
+    }
 }
 
 class Dog extends Animal {
-	public String name;
-	
-	
-	public Dog(String name) {
-		this.name=name;
-	}
 
-	public void sound(int volume) {
-		System.out.println("Dog barks with volume: " + volume);
-	}
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+
+    public void sound(int volume) {
+        System.out.println("Dog barks with volume: " + volume);
+    }
 }
 
 class Cat extends Animal {
 
-	private void sound() {
-		System.out.println("Cat meows");
-	}
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("Cat meows");
+    }
 }
-
-
-
 
 public class Activity3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Dog dog = new Dog("Buddy");
-		dog.sound(5); 
+    public static void main(String[] args) {
+        Dog dog = new Dog("Buddy");
+        dog.sound(5);
+        dog.sound();
 
-		Cat cat = new Cat("Whiskers");
-		cat.sound();
-	}
-
+        Cat cat = new Cat("Whiskers");
+        cat.sound();
+    }
 }
+
+
+// output
+// Dog barks with volume: 5
+// Dog barks
+// Cat meows
